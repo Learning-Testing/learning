65a8070c6262f45c06ea1c2e3648c33f863e8ede
Sean M <scmwhr83@gmail.com>
2019-08-15 22:27:14 -0500

    Create header.h

---
+++ Check_For_Val_In_Vector/header.h
@@ -0,0 +1,8 @@
+#ifndef CLIONTEST_HEADER_H
+#define CLIONTEST_HEADER_H
+
+# include "library.cpp"
+template class check_in_vector<int>;
+template class check_in_vector<std::vector>;
+
+#endif //CLIONTEST_HEADER_H
